a0efa9dc0ecb728c3b4c3b4740c1f3fb786c07cb
Sean Yoon <yoon.s@ufl.edu>
2024-11-26 14:13:34 -0500

    peakFitter.m now working for Bear Spring data

--- STD_water.m
+++ STD_water.m
@@ -1,7 +1,7 @@
 clear all; clc; clf
 %% ========================================================================
 % 
-%                   Standard fluorescein solution samples
+%                   Standard samples processing
 % 
 %                                                      Yoon Research Group
 % -------------------------------------------------------------------------
@@ -10,78 +10,112 @@ clear all; clc; clf
 %        10x: 0.4 ml : 4 ml  -> 11 x
 %         5x: 0.8 ml : 4 ml  ->  6 x
 %         2x:   2 ml : 2 ml  ->  2 x
-% 
 % =========================================================================
 
-publicDir = ['/blue/yoon.s/public/'];
-campaignDir = [publicDir 'BearSpring2023/'];
+campaign = 'SinkRise2024'; % 'SinkRise2024' or 'BearSpring2023'
 
-resultsDir = [pwd '/BearSpring2023/']; 
+archiveDir = sprintf('%s/dyeTracingData/',pwd);
+
+resultsDir = sprintf('%s/processed/%s/',pwd,campaign);
 if ~exist(resultsDir,'dir'); mkdir(resultsDir); end
 
-figDir = [resultsDir 'fig/']; 
+figDir = [resultsDir 'fig/STDs/']; 
 if ~exist(figDir,'dir'); mkdir(figDir); end
 
 %% file name recognition
-dyeType = 'uranine'; % 'SrB', 'uranine', or 'RWT'
-sampleType = 'water';
+sampleType = 'water'; % 'water' or 'eluent'
 
-dataDir = sprintf('%sSTD_%s/',campaignDir, dyeType );
-flNameStruct = dir(dataDir);
-nSample = 0;
-for il = 3:numel(flNameStruct)
-    if strcmp(flNameStruct(il).name(end-2:end),'txt')
-        % if strcmp(flNameStruct(il).name(end-18:end),'_CorrectionData.txt')
+for dyeType = {'uranine'} % 'SrB', 'uranine', or 'RWT'
+    dataDir = sprintf('%s%s/STD_%s/',archiveDir,campaign, dyeType{1} );
+    flNameStruct = dir(dataDir);
+    nSample = 0;
+    for il = 3:numel(flNameStruct)
+        if strcmp(flNameStruct(il).name(end-2:end),'txt')
             nSample = nSample + 1;
             flNameList{nSample,1} = flNameStruct(il).name;
-        % end
+        end
     end
-end
-flNameList_todo = flNameList;
-
-%% standard solutions
-cnc = nan(nSample,1);
-for iSample = 1:nSample
-    flName = flNameList{iSample}(1:end-4);
-    note = textscan(flName, '%s','delimiter','_' ); note = note{1};
-
-    cnc(iSample) = str2double(sprintf('%s.%s',note{2},note{3}));
-    if length(note) >= 4
-        tmp = textscan(note{4},'%s','delimiter','x'); tmp = tmp{1};
-        dilutionFactor(iSample,1) = str2num(tmp{1});
-    else
-        dilutionFactor(iSample,1) = 1;
+    flNameList_todo = flNameList;
+    
+    %% standard solutions
+    cnc = nan(nSample,1);
+    for iSample = 1:nSample
+        flName = flNameList{iSample}(1:end-4);
+        note = textscan(flName, '%s','delimiter','_' ); note = note{1};
+    
+        cnc(iSample) = str2double(sprintf('%s.%s',note{2},note{3}));
+        if length(note) >= 4
+            tmp = textscan(note{4},'%s','delimiter','x'); tmp = tmp{1};
+            dilutionFactor(iSample,1) = str2num(tmp{1});
+        else
+            dilutionFactor(iSample,1) = 1;
+        end
     end
+    dilutionFactor(find(dilutionFactor == 20)) = 21;
+    dilutionFactor(find(dilutionFactor == 10)) = 11;
+    dilutionFactor(find(dilutionFactor == 5)) = 6;
+    
+    %% Peak Fitting
+    area = cell(length(flNameList_todo),1);
+    for iSample = 1:length(flNameList_todo)
+        nIter=10; if cnc(iSample)>1; nIter=50; end
+        area{iSample} = peakFitter(dataDir,figDir,flNameList_todo{iSample},sampleType,nIter);
+        fprintf('sample %d/%d \n',iSample,length(flNameList_todo))
+    end
+    
+    %% standard data analysis
+    area = cell2mat(area);
+    
+    if strcmp(dyeType{1}, 'uranine')
+        area = area(:,1);
+    elseif strcmp(dyeType{1}, 'RWT')
+        area = area(:,2);
+    elseif strcmp(dyeType{1}, 'SrB')
+        area = area(:,3);
+    end
+    
+    area = dilutionFactor.*area;
+        
+    % costF = @(x) sqrt(sum((log10(area) + log10(x) - log10(cnc)).^2));
+    costF = @(x) sqrt(sum((area .* x - cnc).^2));
+    options = optimset('PlotFcns',@optimplotfval,'MaxIter',1000);
+    slope = fminsearch(costF,1e-3,options); 
+
+    SSR = sum((area*slope - cnc).^2); % sum of squared error
+    R2 = 1- SSR / sum((cnc - mean(cnc)).^2); % R^2 value
+
+    flNameSTD = sprintf('%sSTD_%s.mat',resultsDir,dyeType{1} );
+    save(flNameSTD,'area','cnc','slope')
+    
+    %%
+    clf; 
+    subplot(121)
+    plot(area,slope*area,'-'); hold on
+    scatter(area,cnc,'o')
+
+    xl = xlim; yl = ylim;
+    text(xl(1)+diff(xl)/10,yl(2)-diff(yl)/10,sprintf('y = (%.4e) * x',slope))
+    text(xl(1)+diff(xl)/10,yl(2)-diff(yl)/10*2,sprintf('R^2 = %.4f',R2))
+
+    ylabel('concentration [ppb]');
+    xlabel('spectral peak area')
+
+    subplot(122)
+    loglog(area,slope*area,'-'); hold on
+    scatter(area,cnc,'o')
+    
+    ylabel('concentration [ppb]');
+    xlabel('spectral peak area')
+
+    title(sprintf('log-scale'))
+
+    fig = gcf;
+    fig.Units = 'inches';
+    fig.PaperPosition = [0 0 8.5 11];
+
+    flNameFig = sprintf('%s%s_calibration.png',figDir, dyeType{1});
+
+    print(fig,flNameFig,'-dpng')
+
+
 end
-dilutionFactor(find(dilutionFactor == 20)) = 21;
-dilutionFactor(find(dilutionFactor == 10)) = 11;
-dilutionFactor(find(dilutionFactor == 5)) = 6;
-
-%% Peak Fitting
-area = zeros(length(flNameList_todo),1);
-for iSample = 1:length(flNameList_todo)
-    area(iSample) = peakFitter(dataDir,figDir,flNameList_todo{iSample},'tracer',sampleType);
-end
-
-%% standard data analysis
-area = dilutionFactor.*area;
-
-
-% costF = @(x) sqrt(sum((log10(area) + log10(x) - log10(cnc)).^2));
-costF = @(x) sqrt(sum((area .* x - cnc).^2));
-
-options = optimset('PlotFcns',@optimplotfval,'MaxIter',1000);
-slope = fminsearch(costF,1e-3,options); 
-
-%%
-close all
-clf; %scatter(area,cnc); hold on
-subplot(121)
-plot(area,slope*area,'-'); hold on
-scatter(area,cnc,'o')
-subplot(122)
-loglog(area,slope*area,'-'); hold on
-scatter(area,cnc,'o')
-
-title(sprintf('slope: %.3e',slope))
-save([resultsDir 'STD.mat'],'slope')
--- peakFitter.m
+++ peakFitter.m
@@ -1,23 +1,34 @@
-function [area] = peakFitter(dataDir,figDir,flName,dyeType,sampleType)    
+function [area] = peakFitter(dataDir,figDir,flName,sampleType,nIter)    
 
 %% ========================================================================
+% 
 %                          peakFitting function
-%                                                           Seonkyoo Yoon
+% 
+%                                                      Yoon Research Group
 % -------------------------------------------------------------------------
 % This code is to automate the multi-peak fitting process needed for 
 % anlayzing the curves produced by a spectrofluorophotometer. The code 
 % estimates the peak area correpsponding to a dye tracer. 
 % 
-% Currently, this code is designed for fluorescein dye which typically has 
-% the peak center around 508 nm for water and 514 nm for eluant. For 
-% different dye types, the contraint varialbes like A, B, Aeq, Beq, lb, and 
-% ub need to be adjusted. To understand the use of the variables, take a 
-% look at the MATLAB manual page for the function 'fmincon' which is the 
-% optimizer used in this code for the multi-peak fitting.   
+% Currently, this code can handle uranine (fluorescein), sulforhodamine B 
+% (SrB), and rhodamine WT (RWT) which typically have the ranges of peak 
+% emission wavelength range as follows:
+%   - when dissolved in watter
+%       * uranine: 506 - 510 nm
+%       * RWT: 572 - 577 nm
+%       * SrB: 580 - 583 nm
+%   - when dissolved in eluent (i.e., elutant)
+%       * uranine: 514 - 519 nm
+%       * RWT: 564 - 571 nm
+%       * SrB: 575 - 582 nm
+% 
+% This peak fitting code is based the MATLAB non-linear optimization 
+% function 'fmincon', with the contraint varialbes A, B, Aeq, Beq, lb,& ub. 
+% To understand the use of the variables, take a look at the MATLAB manual 
+% page for the function.   
 % 
 % dyeType = currently only 'fluorescein' is avaialbe. 
 % sampleType =  either 'water' or 'eluant'
-% 
 % =========================================================================
     %% data laod
     fidDataFl = fopen([dataDir flName],'r');
@@ -43,14 +54,13 @@ function [area] = peakFitter(dataDir,figDir,flName,dyeType,sampleType)
     
     
     %% contraints according to dye and samples
-    if strcmp(dyeType,'NOM') 
+    if strcmp(sampleType,'water') 
         clc
-        % nPeak = 9;
-        params = [360      390      440                500; ... center
-                   300     10*ones(1,2)        10*ones(1,1); ... height
-                   100     30*ones(1,2)        30*ones(1,1);         ... hwhm
-                   0.8    2*ones(1,2)    30*ones(1,1)];  % shape
-        nPeak = size(params,2);
+        nPeak = 11;
+        params = [ linspace(300, 500, 4) 508 560 582 linspace(600,800,4); ... center
+                   10*ones(1,4)    500 500 500  10*ones(1,4); ... height
+                   100    30*ones(1,3) 10 10 10 30*ones(1,4); ... hwhm
+                    2*ones(1,4)    10  10 10  30*ones(1,4)];  % shape
         params = params(:);
         
         costFnc = @(params) sqrt(mean((intensity-peakSum(wavelength,params)).^2)); 
@@ -67,96 +77,22 @@ function [area] = peakFitter(dataDir,figDir,flName,dyeType,sampleType)
         shape = [zeros(1,nPeak); ... center
                  zeros(1,nPeak); ... height
                  zeros(1,nPeak); ... hwhm
-                 0 2 2   5 ];    % shape
+                 0 2 2 2 10 10 10 5 5 5 5];    % shape
         shape = shape(:);
         
         Aeq = diag(fixed);
         beq = shape;
         
-        lb = [320   370   380   450   ; ... center
-              5*ones(1,nPeak);    ... height
-                0     0     0   0   ;    ... hwhm
-              0.4 zeros(1,nPeak-1);];    % shape
-        ub = [370   450   430   570   ; ... center
-              700*ones(1,nPeak); ... height
-              300   100   100   100  ;    ... hwhm
-              0.9 100*ones(1,nPeak-1);]; % shape
-    elseif strcmp(dyeType,'tracer') 
-        clc
-        nPeak = 9;
-        params = [360 linspace(400, 500, 3) 508 582 linspace(600,740,3); ... center
-                   300    10*ones(1,3)      350 380  10*ones(1,3); ... height
-                   100 30*ones(1,3) 10 10 30*ones(1,3);         ... hwhm
-                   0.8    2*ones(1,3)    10  10  30*ones(1,3)];  % shape
-        params = params(:);
-        
-        costFnc = @(params) sqrt(mean((intensity-peakSum(wavelength,params)).^2)); 
-        
-        A = [];
-        b = [];
-        
-        fixed = [zeros(1,nPeak); ... center
-                 zeros(1,nPeak); ... height
-                 zeros(1,nPeak); ... hwhm
-                 0 ones(1,nPeak-1) ];    % shape
-        fixed = fixed(:);
-        
-        shape = [zeros(1,nPeak); ... center
-                 zeros(1,nPeak); ... height
-                 zeros(1,nPeak); ... hwhm
-                 0 2 2 2 10 10 10 5 5];    % shape
-        shape = shape(:);
-        
-        Aeq = diag(fixed);
-        beq = shape;
-        
-        lb = [320   370   380   420   508  580  510   550   550; ... center
+        lb = [320   370   380   420   506 555  580  600*ones(1,4); ... center
               zeros(1,nPeak);    ... height
-                0     0     0     0     5     5     0     0     0;    ... hwhm
+              5*ones(1,nPeak);    ... hwhm
               0.4 zeros(1,nPeak-1);];    % shape
-        ub = [370   450   420   480   509  584  540   780   780; ... center
-              700*ones(1,nPeak); ... height
-              300   100   100   100    15   15  100   100   100;    ... hwhm
-              0.9 100*ones(1,nPeak-1);]; % shape
-    elseif strcmp(dyeType,'uranine') && strcmp(sampleType,'water')
-        clc
-        nPeak = 8;
-        params = [360 linspace(400, 500, 3) 508 linspace(600,740,3); ... center
-                   300    10*ones(1,3)   350   10*ones(1,3); ... height
-                   100 30*ones(1,3) 10 30*ones(1,3);         ... hwhm
-                   0.8    2*ones(1,3)    10    30*ones(1,3)];  % shape
-        params = params(:);
-        
-        costFnc = @(params) sqrt(mean((intensity-peakSum(wavelength,params)).^2)); 
-        
-        A = [];
-        b = [];
-        
-        fixed = [zeros(1,nPeak); ... center
-                 zeros(1,nPeak); ... height
-                 zeros(1,nPeak); ... hwhm
-                 0 ones(1,7) ];    % shape
-        fixed = fixed(:);
-        
-        shape = [zeros(1,nPeak); ... center
-                 zeros(1,nPeak); ... height
-                 zeros(1,nPeak); ... hwhm
-                 0 2 2 2 10 10 5 5];    % shape
-        shape = shape(:);
-        
-        Aeq = diag(fixed);
-        beq = shape;
-        
-        lb = [320   370   380   420   508   550   550   550; ... center
-              zeros(1,nPeak);    ... height
-                0     0     0     0     5     0     0     0;    ... hwhm
-              0.4 zeros(1,nPeak-1);];    % shape
-        ub = [370   450   420   450   509   600   780   780; ... center
-              inf*ones(1,nPeak); ... height
-              300   100   100   100    15   100   100   100;    ... hwhm
+        ub = [350   450   420   480   510  565 584  850*ones(1,4); ... center
+              700*ones(1,4) inf*ones(1,3) 700*ones(1,4);... height
+              300   100   100   100    15   15  15  100   100   100 100;    ... hwhm
               0.9 100*ones(1,nPeak-1);]; % shape
     
-    elseif strcmp(dyeType,'uranine') && strcmp(sampleType,'eluant')
+    elseif strcmp(sampleType,'eluent')
         clc
         nPeak = 8;
         params = [360 linspace(400, 500, 3) 514 linspace(600,740,3); ... center
@@ -196,108 +132,106 @@ function [area] = peakFitter(dataDir,figDir,flName,dyeType,sampleType)
     
     end
     %% parameter fitting using fmincon
-    options = optimoptions('fmincon','Display','iter');%'MaxFunctionEvaluations',1e+10,'MaxIterations',1e+5);
+    % options = optimoptions('fmincon','Display','off');%'MaxFunctionEvaluations',1e+10,'MaxIterations',1e+5);
         
-    for iter =1:10
+    for iter =1:nIter
+        fprintf('%d/%d',iter,nIter)
         params = fmincon(costFnc,params,A,b,Aeq,beq,lb,ub);
-    end
+        [curve] = peakSum(wavelength,params);
     
-    %% visualization
-    clf; 
-    paramsPrint = reshape(params,4,nPeak)';
-    [curve] = peakSum(wavelength,params);
-
-
-    % clr = 'kkkkrbkkk';
-    clr = 'kkkkkkkkk';
-
-    subplot(100,1,1:40)
-    plot(wavelength,intensity); hold on
-    plot(wavelength,curve)
-
-    for iPeak = 0:nPeak-1
-        if 1%params(4*iPeak+2) > 1
-            peak = Pearson7(wavelength,params(4*iPeak+1:4*iPeak+4));
-            plot(wavelength,peak,[clr(iPeak+1) ':']); hold on
+    
+        %% visualization
+        clf; 
+        paramsPrint = reshape(params,4,nPeak)';
+        [curve] = peakSum(wavelength,params);
+    
+    
+        % clr = 'kkkkrbkkk';
+        clr = 'kkkkkkkkkkkkkkkk';
+    
+        subplot(100,1,1:40)
+        plot(wavelength,intensity); hold on
+        plot(wavelength,curve)
+    
+        for iPeak = 0:nPeak-1
+            if 1%params(4*iPeak+2) > 1
+                peak = Pearson7(wavelength,params(4*iPeak+1:4*iPeak+4));
+                plot(wavelength,peak,[clr(iPeak+1) ':']); hold on
+            end
+        end
+    
+        title(flName,'Interpreter','none')
+    
+        subplot(100,1,46:55)
+        plot(wavelength,intensity-curve); hold on
+        plot(wavelength,zeros(size(intensity)))
+    
+        yl = ylim;
+        xl = xlim;
+    
+        for iPeak = 0:nPeak-1
+            plot([params(4*iPeak+1) params(4*iPeak+1)],[-15 15],clr(iPeak+1));      
+        end
+        xlim(xl)
+        ylim([-7 7])
+    
+    
+        SSR = sum((curve - intensity).^2); % sum of squared error
+        R2 = 1- SSR / sum((intensity - mean(intensity)).^2);
+        weigth = 1./max(sqrt(intensity), ones(size(intensity)));
+        WSSR = sum(weigth.^2.*(curve - intensity).^2); % weighted sum of squared error
+    
+        % degree of freedom = data dimensiton - #. parameter + #. fixed parameter
+        DoF = length(intensity) - 4*nPeak + sum(fixed); 
+    
+        subplot(100,1,61:100)
+        yl = ylim;
+    
+        txt = sprintf('1 sigma = %.2f   [sqrt(WSSR/DoF)] ', sqrt(WSSR/DoF));
+        text(0,yl(2),txt,'fontsize',12)
+    
+        txt = sprintf('WSSR=%.4f   DoF=%d   WSSR/DoF=%.4f   SSR=%.4f   R2=%.4f', ...
+            WSSR,DoF,WSSR/DoF,SSR,R2);
+        text(0,yl(2)-diff(yl)/(nPeak+1),txt,'fontsize',12)
+    
+    
+        txt = sprintf('%10s %12s %12s %12s %12s %12s \n',...
+                        'peak', 'center','height','hwhm','shape','area');
+    
+        yl = ylim;
+        text(0,yl(2)-2*diff(yl)/(nPeak+1),txt,'fontsize',12)
+        area = [];
+        for iPeak = 0:nPeak-1
+            if iPeak >= 4 && iPeak <= 6
+                peak = Pearson7(wavelength,params(4*iPeak+1:4*iPeak+4));
+                area = [area trapz(wavelength,peak)];
+                % area_ = Pearson7_Area(params(4*iPeak+1:4*iPeak+4));
+    
+                txt = sprintf('%10d %12.3f %12.3f %12.3f %12.3f %12.3f\n', ...
+                iPeak+1, paramsPrint(iPeak+1,:), area(end));
+            else
+                txt = sprintf('%10d %12.3f %12.3f %12.3f %12.3f \n', ...
+                iPeak+1, paramsPrint(iPeak+1,:));
+            end
+            text(0,yl(2)-(iPeak+3)*diff(yl)/(nPeak+1),txt,'fontsize',12)
+    
+        end
+    
+        % tmp = [wavelength peak ]
+        axis off
+    
+        drawnow;
+    
+        if iter ==nIter
+            fig = gcf;
+            fig.Units = 'inches';
+            fig.PaperPosition = [0 0 8.5 11];
+        
+            flNameFig = sprintf([figDir flName(1:end-4) '.png']);
+        
+            print(fig,flNameFig,'-dpng')
         end
     end
-
-    title(flName,'Interpreter','none')
-
-    subplot(100,1,46:55)
-    plot(wavelength,intensity-curve); hold on
-    plot(wavelength,zeros(size(intensity)))
-
-    yl = ylim;
-    xl = xlim;
-
-    for iPeak = 0:nPeak-1
-        if 1%params(4*iPeak+2) > 1
-            plot([params(4*iPeak+1) params(4*iPeak+1)],[-15 15],clr(iPeak+1));  
-        end
-    end
-    xlim(xl)
-    ylim([-7 7])
-
-
-    SSR = sum((curve - intensity).^2); % sum of squared error
-    R2 = 1- SSR / sum((intensity - mean(intensity)).^2);
-    weigth = 1./max(sqrt(intensity), ones(size(intensity)));
-    WSSR = sum(weigth.^2.*(curve - intensity).^2); % weighted sum of squared error
-
-    % degree of freedom = data dimensiton - #. parameter + #. fixed parameter
-    DoF = length(intensity) - 4*nPeak + sum(fixed); 
-
-    subplot(100,1,61:100)
-    yl = ylim;
-
-    txt = sprintf('1 sigma = %.2f   [sqrt(WSSR/DoF)] ', sqrt(WSSR/DoF));
-    text(0,yl(2),txt,'fontsize',12)
-
-    txt = sprintf('WSSR=%.4f   DoF=%d   WSSR/DoF=%.4f   SSR=%.4f   R2=%.4f', ...
-        WSSR,DoF,WSSR/DoF,SSR,R2);
-    text(0,yl(2)-diff(yl)/(nPeak+1),txt,'fontsize',12)
-
-
-    txt = sprintf('%10s %12s %12s %12s %12s %12s \n',...
-                    'peak', 'center','height','hwhm','shape','area');
-
-    yl = ylim;
-    text(0,yl(2)-2*diff(yl)/(nPeak+1),txt,'fontsize',12)
-    area = [];
-    for iPeak = 0:nPeak-1
-        if 1%params(4*iPeak+2)>1
-        if iPeak == 4
-            peak = Pearson7(wavelength,params(4*iPeak+1:4*iPeak+4));
-            area = [area trapz(wavelength,peak)];
-
-            area_ = Pearson7_Area(params(4*iPeak+1:4*iPeak+4));
-
-            txt = sprintf('%10d %12.3f %12.3f %12.3f %12.3f %12.3f\n', ...
-            iPeak+1, paramsPrint(iPeak+1,:), area(end));
-        else
-            txt = sprintf('%10d %12.3f %12.3f %12.3f %12.3f \n', ...
-            iPeak+1, paramsPrint(iPeak+1,:));
-        end
-        text(0,yl(2)-(iPeak+3)*diff(yl)/(nPeak+1),txt,'fontsize',12)
-        end
-    end
-
-    tmp = [wavelength peak ]
-    axis off
-
-    drawnow;
-
-    %
-    fig = gcf;
-    fig.Units = 'inches';
-    fig.PaperPosition = [0 0 8.5 11];
-
-    flNameFig = sprintf([figDir flName(1:end-4) '.png']);
-
-    print(fig,flNameFig,'-dpng')
-
-
 end
 
 %% sub-functions
